982e4ff7af8dfed6dcaea93ce2e81be4d216cbcd
priyaanka7 <104008176+priyaanka7@users.noreply.github.com>
2022-06-12 23:08:25 +0530

    Add files via upload

---
+++ 21102063_PRIYANKA_ASS4_OUTPUTS.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\Priyanka\AppData\Local\Programs\Python\Python310\ass4.py ==
+Question1
+Enter Your Marks:89
+Your Grade is A
+
+
+Question2
+Enter a Year:2022
+It is not a leap year
+
+
+Question3
+10 random multiple questions
+First number is: 2
+First number is: 4
+Your answer is:8
+Your answer is right
+First number is: 7
+First number is: 3
+Your answer is:21
+Your answer is right
+First number is: 16
+First number is: 6
+Your answer is:67
+Your answer is wrong 
+ The correct answer is: 96
+First number is: 1
+First number is: 10
+Your answer is:78
+Your answer is wrong 
+ The correct answer is: 10
+First number is: 19
+First number is: 8
+Your answer is:8
+Your answer is wrong 
+ The correct answer is: 152
+First number is: 9
+First number is: 5
+Your answer is:45
+Your answer is right
+First number is: 11
+First number is: 8
+Your answer is:88
+Your answer is right
+First number is: 5
+First number is: 4
+Your answer is:20
+Your answer is right
+First number is: 19
+First number is: 2
+Your answer is:56
+Your answer is wrong 
+ The correct answer is: 38
+First number is: 27
+First number is: 4
+Your answer is:89
+Your answer is wrong 
+ The correct answer is: 108
+
+
+Question4
+The number of candies in a halloween bowl are: 177
---
+++ 21102063_PRIYANKA_ASS4_PROGRAMS.py
@@ -0,0 +1,58 @@
+#Program for Question 1
+print("Question1")
+a=int(input("Enter Your Marks:"))
+if a<25:
+    print("Your Grade is F")
+elif 25<a<45:
+    print("Your Grade is E")
+elif 45<a<50:
+    print("Your Grade is D")
+elif 50<a<60:
+    print("Your Grade is C")
+elif 60<a<80:
+    print("Your Grade is B")
+else :
+    print("Your Grade is A")
+print("\n")
+
+
+
+#Program for Question 2
+print("Question2")
+a=int(input("Enter a Year:"))
+if (a%4==0 and a%100!=0) or a%400==0:
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
+print("\n")
+
+
+
+#Program for Question 3
+print("Question3")
+import random
+print("10 random multiple questions")
+n=0
+for n in range (0,10):
+    a=random.randint(1,30)
+    b=random.randint(1,10)
+    print("First number is:",a)
+    print("First number is:",b)
+    c=a*b
+    d=int(input("Your answer is:"))
+    if d==c:
+          print("Your answer is right")
+    else:
+          print("Your answer is wrong \n The correct answer is:",c)
+    n+=1
+print("\n")
+
+
+
+#Program for Question 4
+print("Question4")
+a=1
+while a<200:
+    if (a%5==2) and (a%6==3) and (a%7==2):
+        print("The number of candies in a halloween bowl are:",a)
+    a+=1
