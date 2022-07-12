524ac0b686050c7c088204197d73f815f9a25199
priyaanka7 <104008176+priyaanka7@users.noreply.github.com>
2022-07-12 12:53:07 +0530

    Update ass6ans.py

--- ass6ans.py
+++ ass6ans.py
@@ -1,15 +1,64 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-== RESTART: C:/Users/Priyanka/AppData/Local/Programs/Python/Python310/ass6.py ==
+== RESTART: C:\Users\Priyanka\AppData\Local\Programs\Python\Python310\ass6.py ==
 Question1
-Enter a number: 24
- 24 is not a Perfect Number
+Enter a number: 6
+ 6 is a Perfect Number
 
 
 Question2
-enter a word: naman
+Enter text:naman
+palindrome
+
+
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+Question4
 Yes
 
 
-Question3
+Question5
+Enter: frog-cow-butterfly-piegon-cat
+butterfly-cat-cow-frog-piegon
+
+
+Question6
+
+Student ID: 21102063
+Student Name: Priyanka
+
+Student Name: Priyanka
+Student Class: Civil
+
+Student ID: 21102033
+Student Name: Chhavi
+
+Student Name: Chhavi
+Student Class: Civil
+
+
+Question7
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class :
+True
+True
+
+
+Question8
+-10 2 8
+-7 -3 10
+
+
+Question9
+enter : ()
+() - is balanced
